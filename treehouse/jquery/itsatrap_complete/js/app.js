13b5a024198c6aeb56cb018d7aeb1f3ccd710160
Gwan Yip <gwan.yip26@gmail.com>
2014-04-09 14:43:02 -0400

    Update treehouse folders with illustrator, jquery, jquer_mobile and ruby/methods

---
+++ treehouse/jquery/itsatrap_complete/js/app.js
@@ -0,0 +1,8 @@
+// //Hide Warning
+// //Show Warning Slowly
+// $(".warning").hide().show("slow");
+
+$(".warning").hide().show("slow");
+
+
+
---
+++ treehouse/ruby/methods/methods.rb
@@ -0,0 +1,26 @@
+def hello(name1,name2)
+	puts "Hello #{name1}"
+	puts "Hello #{name2}"
+end
+
+hello("Gwan","Ben")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
